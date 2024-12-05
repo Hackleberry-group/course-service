c7a83479428862bec76f67c750a3be714d3332cf
Joshua Andrea <joshua.andrea@hotmail.com>
2024-12-05 14:49:35 +0100

    modules controller complete

--- CourseServiceAPI/Controllers/ModulesController.cs
+++ CourseServiceAPI/Controllers/ModulesController.cs
@@ -1,6 +1,7 @@
 ﻿using CourseServiceAPI.Helpers;
 using CourseServiceAPI.Interfaces;
 using CourseServiceAPI.Models.Module.DTOs;
+using CourseServiceAPI.Models.Topic.DTOs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CourseServiceAPI.Controllers
@@ -11,11 +12,13 @@ namespace CourseServiceAPI.Controllers
     {
         private readonly ILogger<ModulesController> _logger;
         private readonly IModuleService _moduleService;
+        private readonly ITopicService _topicService;
 
-        public ModulesController(ILogger<ModulesController> logger, IModuleService moduleService)
+        public ModulesController(ILogger<ModulesController> logger, IModuleService moduleService, ITopicService topicService)
         {
             _logger = logger;
             _moduleService = moduleService;
+            _topicService = topicService;
         }
 
         [HttpGet]
@@ -25,12 +28,42 @@ namespace CourseServiceAPI.Controllers
             return modules.Select(Mapper.MapToModuleResponseDto);
         }
 
-        [HttpGet("")]
+        [HttpGet("{id}")]
+        public async Task<ModuleResponseDTO> GetModuleById(Guid id)
+        {
+            var module = await _moduleService.GetModuleByIdAsync(id);
+            return Mapper.MapToModuleResponseDto(module);
+        }
+
+        [HttpGet("{id}/topics")]
+        public async Task<IEnumerable<TopicResponseDto>> GetTopicsByModuleId(Guid moduleId)
+        {
+            var topics = await _topicService.GetTopicsByModuleIdAsync(moduleId);
+            return topics.Select(Mapper.MapToTopicResponseDto);
+        }
 
         [HttpPost]
+        public async Task<ModuleResponseDTO> Post([FromBody] ModuleRequestDTO moduleDTO)
+        {
+            var module = Mapper.MapToModule(moduleDTO);
+            var createdModule = await _moduleService.CreateModuleAsync(module);
+
+            return Mapper.MapToModuleResponseDto(createdModule);
+        }
 
         [HttpPut]
+        public async Task<ModuleResponseDTO> Put([FromBody] ModuleRequestDTO moduleDTO)
+        {
+            var module = Mapper.MapToModule(moduleDTO);
+            var updatedModule = await _moduleService.PutModuleByIdAsync(module);
+
+            return Mapper.MapToModuleResponseDto(updatedModule);
+        }
 
         [HttpDelete]
+        public async Task Delete(Guid id)
+        {
+            await _moduleService.DeleteModuleAsync(id);
+        }
     }
 }
