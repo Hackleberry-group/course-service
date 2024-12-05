09244a276c1bf1e16ab73244eca8499608f3a667
Joshua Andrea <joshua.andrea@hotmail.com>
2024-12-05 14:49:48 +0100

    data annotations for course dtos

--- CourseServiceAPI/Models/Course/DTOs/CourseRequestDTO.cs
+++ CourseServiceAPI/Models/Course/DTOs/CourseRequestDTO.cs
@@ -1,9 +1,14 @@
-﻿namespace CourseServiceAPI.Models.Course.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseServiceAPI.Models.Course.DTOs
 {
     public class CourseRequestDTO
     {
+        [Required]
         public string Name { get; init; }
+        [Required]
         public string ProgrammingLanguage { get; init; }
+        [Required]
         public string TeacherId { get; init; }
     }
 }
--- CourseServiceAPI/Models/Course/DTOs/CourseResponseDTO.cs
+++ CourseServiceAPI/Models/Course/DTOs/CourseResponseDTO.cs
@@ -1,10 +1,16 @@
-﻿namespace CourseServiceAPI.Models.Course.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseServiceAPI.Models.Course.DTOs
 {
     public class CourseResponseDTO
     {
+        [Required]
         public Guid Id { get; init; }
+        [Required]
         public string Name { get; init; }
+        [Required]
         public string ProgrammingLanguage { get; init; }
+        [Required]
         public string TeacherId { get; init; }
     }
 }
